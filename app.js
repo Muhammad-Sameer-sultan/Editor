b1f10f32529fbbb195c3dc682a68c3aa6680369c
sameer <sameersultan1001@gmail.com>
2023-08-24 15:47:19 +0500

    editor complete

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-let optionsButtons = document.querySelectorAll(".option-button");
-let advancedOptionButton = document.querySelectorAll(".adv-option-button");
 let fontName = document.getElementById("fontName");
 let fontSizeRef = document.getElementById("fontSize");
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
 let writingArea = document.getElementById("text-input");
 let linkButton = document.getElementById("createLink");
+let unLinkButton = document.getElementById("unlink");
 let alignButtons = document.querySelectorAll(".align");
 let spacingButtons = document.querySelectorAll(".spacing");
 let formatButtons = document.querySelectorAll(".format");
@@ -20,84 +21,48 @@ let fontList = [
   "cursive",
 ];
 
-//Initial Settings
+// main function run on window load
 const initializer = () => {
-  //function calls for highlighting buttons
-  //No highlights for link, unlink,lists, undo,redo since they are one time operations
   highlighter(alignButtons, true);
+  highlighter(scriptButtons, true);
   highlighter(spacingButtons, true);
   highlighter(formatButtons, false);
-  highlighter(scriptButtons, true);
 
-  //create options for font names
+  // font style
   fontList.map((value) => {
-    let option = document.createElement("option");
-    option.value = value;
-    option.innerHTML = value;
-    fontName.appendChild(option);
+    let options = document.createElement("option");
+    options.value = value;
+    options.innerHTML = value;
+    fontName.appendChild(options);
   });
-
-  //fontSize allows only till 7
-  for (let i = 1; i <= 7; i++) {
+  for (let i = 1; i < 8; i++) {
     let option = document.createElement("option");
     option.value = i;
     option.innerHTML = i;
     fontSizeRef.appendChild(option);
   }
-
-  //default size
-  fontSizeRef.value = 3;
+  fontSizeRef.value = 5;
+  // font size end
 };
 
-//main logic
-const modifyText = (command, defaultUi, value) => {
-  //execCommand executes command on selected text
-  document.execCommand(command, defaultUi, value);
+
+
+//  functions
+
+// main function
+const modifyText = (command, defaultUI, value) => {
+  document.execCommand(command, defaultUI, value);
 };
-
-//For basic operations which don't need value parameter
-optionsButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    modifyText(button.id, false, null);
-  });
-});
-
-//options that require value parameter (e.g colors, fonts)
-advancedOptionButton.forEach((button) => {
-  button.addEventListener("change", () => {
-    modifyText(button.id, false, button.value);
-  });
-});
-
-//link
-linkButton.addEventListener("click", () => {
-  let userLink = prompt("Enter a URL");
-  //if link has http then pass directly else add https
-  if (/http/i.test(userLink)) {
-    modifyText(linkButton.id, false, userLink);
-  } else {
-    userLink = "http://" + userLink;
-    modifyText(linkButton.id, false, userLink);
-  }
-});
-
 //Highlight clicked button
 const highlighter = (className, needsRemoval) => {
   className.forEach((button) => {
     button.addEventListener("click", () => {
       //needsRemoval = true means only one button should be highlight and other would be normal
       if (needsRemoval) {
-        let alreadyActive = false;
-
-        //If currently clicked button is already active
         if (button.classList.contains("active")) {
-          alreadyActive = true;
-        }
-
-        //Remove highlight from other buttons
-        highlighterRemover(className);
-        if (!alreadyActive) {
-          //highlight clicked button
+          highlighterRemover(className);
+        } else {
+          highlighterRemover(className);
           button.classList.add("active");
         }
       } else {
@@ -107,11 +72,50 @@ const highlighter = (className, needsRemoval) => {
     });
   });
 };
-
+// remove button from higght light
 const highlighterRemover = (className) => {
   className.forEach((button) => {
     button.classList.remove("active");
   });
 };
+
+// remove highlight button
+
+//  <========== Button Click response ==============>
+
+// //For basic operations which don't need value parameter
+
+optionsButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    modifyText(button.id, false, null);
+    // button.style.backgroundColor="red"
+  });
+});
+
+// For basic operations which don't need value parameter
+advancedOptionButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    modifyText(button.id, false, button.value);
+    // button.style.backgroundColor="red"
+  });
+});
+
+// For basic operations which  need value linkes button
+linkButton.addEventListener('click',()=>{
+  let url =prompt("Enter Url");
+  if(/http/i.test(url)){
+
+    modifyText(linkButton.id,false,url)
+  }
+  else{
+    url="http://"+url;
+    modifyText(linkButton.id,false,url)
+  }
+})
+// Unlink
+
+unLinkButton.addEventListener('click',()=>{
+  modifyText(unLinkButton.id,true,null)
+})
 
 window.onload = initializer();
